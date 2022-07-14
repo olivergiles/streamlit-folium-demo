060f1c74ec15510bfe3c4dd541dee68c7caa35d3
Oliver Giles <ogiles@email.com>
2022-07-14 10:00:26 +0200

    completed app with doc strings

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from streamlit_folium import st_folium
+import folium
+import requests
+
+def title() -> None:
+    """ Add title to streamlit page.
+    """
+    st.title("Streamlit folium demo!")
+
+def coordinates(address: str) -> tuple:
+    """ Get coords of a given address.
+
+    Args:
+        address (str): A string of the address you want to display.
+
+    Returns:
+        tuple: A tuple of lat and lon as floats.
+    """
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {
+        'q': address,
+        'format': 'json'
+    }
+    response = requests.get(url, params=params).json()
+    return (response[0]['lat'], response[0]['lon'])
+
+def plot_map(lat: float, lon: float) -> None:
+    """ Given a latitude and longitude display a folium map on streamlit page.
+
+    Args:
+        lat (float): Latitude.
+        lon (float): Longitude.
+    """
+    m = folium.Map(location=[lat, lon], zoom_start=16)
+    folium.Marker(
+    location=[lat, lon],
+    popup="Your coords!",
+    icon=folium.Icon(color="red", icon="info-sign"),
+    ).add_to(m)
+    st_folium(m, width = 725)
+
+def user_input() -> None:
+    """ Takes user input of an address.
+    """
+    address = st.text_input('Where do you want a map of?')
+    if address:
+        lat, lon = coordinates(address)
+        plot_map(lat, lon)
+
+def app() -> None:
+    """ Call the components of the streamlit app.
+    """
+    title()
+    user_input()
+
+if __name__ == "__main__":
+    app()
